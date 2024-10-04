15cff60af28b61b2f16cde9bbea12f71d31a651c
A701\1dam <raildetrenes@gmail.com>
2024-10-04 16:26:07 +0200

    no message

--- src/main/java/org/example/Ejercicio2.java
+++ src/main/java/org/example/Ejercicio2.java
@@ -1,42 +1,63 @@
 package org.example;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Ejercicio2 {
     public void ejercicio2() {
-        System.out.print("Ingrese tu nombre: ");
-        Scanner entrada = new Scanner(System.in);
-        String nombre = entrada.nextLine();
-        System.out.println("La obra de " + nombre);
-        System.out.println("Introduce el ancho de la pared");
-        float ancho1 = entrada.nextInt();
-        System.out.println("Introduce el altura de la pared");
-        float altura1 = entrada.nextInt();
-        System.out.println("Introduce el ancho del azulejo");
-        float ancho2 = entrada.nextInt();
-        System.out.println("Introduce el altura del azulejo");
-        float altura2 = entrada.nextInt();
-        float azulejo = 0;
+        boolean hola = true;
+        int numero = 0;
+        if (hola == true) {
+            try {
+                System.out.print("Ingrese tu nombre: ");
+                Scanner entrada = new Scanner(System.in);
+                String nombre = entrada.nextLine();
+                System.out.println("La obra de " + nombre);
+                System.out.println("Introduce el ancho de la pared");
+                float ancho1 = entrada.nextInt();
+                System.out.println("Introduce el altura de la pared");
+                float altura1 = entrada.nextInt();
+                System.out.println("Introduce el ancho del azulejo");
+                float ancho2 = entrada.nextInt();
+                System.out.println("Introduce el altura del azulejo");
+                float altura2 = entrada.nextInt();
+            } catch (InputMismatchException error) {
+                System.out.println("Pon Numeros");
+                System.exit(0);
+            }
+            System.out.print("Ingrese tu nombre: ");
+            Scanner entrada = new Scanner(System.in);
+            String nombre = entrada.nextLine();
+            System.out.println("La obra de " + nombre);
+            System.out.println("Introduce el ancho de la pared");
+            float ancho1 = entrada.nextInt();
+            System.out.println("Introduce el altura de la pared");
+            float altura1 = entrada.nextInt();
+            System.out.println("Introduce el ancho del azulejo");
+            float ancho2 = entrada.nextInt();
+            System.out.println("Introduce el altura del azulejo");
+            float altura2 = entrada.nextInt();
+            float azulejo = 0;
 
-        if (ancho1 < 1 || ancho2 < 1 || altura1 <1 || altura2 <1 )
-        {System.out.println("El azulejo o la pared no puede medir 0 o menos en todas sus direcciones");
-        System.exit(0);}
+            if (ancho1 < 1 || ancho2 < 1 || altura1 < 1 || altura2 < 1) {
+                System.out.println("El azulejo o la pared no puede medir 0 o menos en todas sus direcciones");
+                System.exit(0);
+            }
 
-        if (ancho2 == altura2) {
-            System.out.println("El azulejo no puede ser cuadrado");
-            System.exit(0);
-        }
-        if ( ancho1 * altura1 < ancho2 * altura2 )
-        {
-            System.out.println("El azulejo no puede ser mas grande que la pared");
-            System.exit(0);
-        }
-        if ( ancho1 * altura1 > ancho2 * altura2 )
-        {
-            azulejo = ((ancho1 * altura1) / (ancho2 * altura2));
-                    System.out.println("Se necesitan " + azulejo + " azulejos");
-        }
+            if (ancho2 == altura2) {
+                System.out.println("El azulejo no puede ser cuadrado");
+                System.exit(0);
+            }
+            if (ancho1 * altura1 < ancho2 * altura2) {
+                System.out.println("El azulejo no puede ser mas grande que la pared");
+                System.exit(0);
+            }
+            if (ancho1 * altura1 > ancho2 * altura2) {
+                azulejo = ((ancho1 * altura1) / (ancho2 * altura2));
+                System.out.println("Se necesitan " + azulejo + " azulejos");
+            }
 
-        //Lo que hace el programa es calcular los azulejos que necesitamos para una pared, dandole nosotros el ancho y el alto de cada atributo, si algun atributo es 0, el azulejo es cuadrado o el azulejo es mas grande que la pared, nos saldra del programa, sino nos dira cuantos azulejos necesitamos para la pared
+            //Lo que hace el programa es calcular los azulejos que necesitamos para una pared, dandole nosotros el ancho y el alto de cada atributo, si algun atributo es 0, el azulejo es cuadrado o el azulejo es mas grande que la pared, nos saldra del programa, sino nos dira cuantos azulejos necesitamos para la pared
+        }
     }
 }
